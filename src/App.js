a575664ab206d9d2ed713dac0ae7252333a1a067
Borwonluck <warawuth52@gmail.com>
2025-05-12 16:01:31 +0700

    Add AddForm component: implement student input form with validation and styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@
 import Header from "./components/Header";
 import StudetList from "./components/StudentList";
 import "./App.css";
+import AddForm from "./components/AddForm";
 import { useState } from "react";
 
 function App() {
   //สร้าง State
     const [students, setStudent] = useState([
       { id: 1, name: "วอน" },
-      { id: 2, name: "เมล" },
-      { id: 3, name: "โลนี่" },
     ]);
 
     function deleteStudent(id) {
@@ -23,6 +22,7 @@ function App() {
 
         <Header title="ข้อมูลนักเรียน"/>
         <main>
+          <AddForm/>
           <StudetList students={students} deleteStudent={deleteStudent}/>
         </main>
       </div>
---
+++ src/components/AddForm.js
@@ -0,0 +1,29 @@
+﻿import { useState } from "react";
+import "./AddForm.css";
+
+export default function AddForm(){
+    const [name, setName] = useState("");
+    function saveStudent(e){
+        e.preventDefault();
+        if(!name){
+            alert("กรุณาป้อนข้อมูล");
+        }
+        else{
+            const newStudent={
+                id:Math.floor(Math.random()*1000),
+                name:name
+            }
+            console.log(newStudent);
+        }
+        
+    }
+    return(
+        <section className="container">
+            <form onSubmit={saveStudent}>
+                <label>ชื่อนักเรียน</label>
+                <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                <button type="submit" className="btn-add">บันทึก</button>
+            </form>
+        </section>
+    );
+}
